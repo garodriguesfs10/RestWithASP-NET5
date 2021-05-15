422525f45c43579906e20d0b14e5bed6ed35d315
Gabriel Rodrigues <garodriguesfs10@gmail.com>
2021-05-15 15:08:58 -0300

    soma

---
+++ 01_RestWithASPNET_ScaffoldVisualStudio/RestWithASP-NET5/RestWithASP-NET5/Controllers/CalculatorController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+      
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstnumber}/{secondnumber}")]
+        public IActionResult Get(string firstnumber, string secondnumber)
+        {
+            if (isNumeric(firstnumber) && isNumeric(secondnumber))
+            {
+                var sum = Convert.ToDecimal(firstnumber) + Convert.ToDecimal(secondnumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Request");
+
+        }
+
+        private bool isNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber, 
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+            return isNumber;
+
+            throw new NotImplementedException();
+        }
+    }
+}
